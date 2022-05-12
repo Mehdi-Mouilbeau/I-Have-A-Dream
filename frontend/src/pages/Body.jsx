40922a8011577181cefde84c7388f9c7f3cff0ca
Galatée Lardière <galatee.lardiere@gmail.com>
2022-05-12 17:37:44 +0200

    Create DataBase

--- frontend/src/pages/Body.jsx
+++ frontend/src/pages/Body.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Partages from "@pages/Partages";
 import Home from "@pages/Home";
 import Annonce from "@pages/Annonce";
 import Profil from "@pages/Profil";
-import { Route, Routes } from "react-router-dom";
 
 export default function Body() {
   return (
     <div>
-      <Routes>
+      <BrowserRouter>
         <Route path="/" element={<Home />} />
         <Route path="/partages" element={<Partages />} />
         <Route path="/annonce" element={<Annonce />} />
         <Route path="/monprofil" element={<Profil />} />
-      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
