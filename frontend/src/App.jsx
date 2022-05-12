5fda67fd2304794e649d70a26a31e69769923bcc
Nicolas AUVRAY <nicolas-auvray_student2022@wilder.school>
2022-05-12 17:45:39 +0200

    création du composant Que recherchez-vous ?

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import Navbar from "@components/Navbar";
-import Footer from "@components/Footer";
 import Body from "@pages/Body";
+import Footer from "@components/Footer";
+import Navbar from "@components/Navbar";
+
 import "./App.css";
 
 function App() {
