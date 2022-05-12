be76174d94ee24e8026c9a585955dbab85c8dbf8
Galatée Lardière <galatee.lardiere@gmail.com>
2022-05-12 18:11:19 +0200

    first div form

--- frontend/src/pages/Annonce.jsx
+++ frontend/src/pages/Annonce.jsx
@@ -1,3 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
 export default function Annonce() {
-  return <div>Déposer une annonce</div>;
+  return (
+    <div>
+      <Navbar />
+      Déposer une annonce
+      <div className="placeAnAd">ffff</div>
+    </div>
+  );
 }
