f3ec86a39f90a550c04dcffa9eca4d9b31cb9c3f
alizeehamon <alizee.hamonpro@gmail.com>
2022-05-12 20:50:22 +0200

    création composant card user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Body from "@pages/Body";
 import Footer from "@components/Footer";
 import Navbar from "@components/Navbar";
--- frontend/src/pages/Profil.jsx
+++ frontend/src/pages/Profil.jsx
@@ -1,3 +1,10 @@
+import React from "react";
+import User from "../components/User";
+
 export default function Profil() {
-  return <div>Mon compte</div>;
+  return (
+    <div>
+      <User />
+    </div>
+  );
 }
