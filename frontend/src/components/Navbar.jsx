11d20c3d07ac50e6a571d7474a6ee5f5f96c89d5
Aline Saint-Lanne <aline.saint-lanne@orange.fr>
2022-05-12 17:42:20 +0200

    banniere

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../assets/logo.png";
+
 export default function Navbar() {
-  return <div>Navigation</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <h1 className="title"> I have a green ! </h1>
+    </div>
+  );
 }
