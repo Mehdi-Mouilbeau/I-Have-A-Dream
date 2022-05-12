e37f07c42f4df999612ce7c1b9b319ff7737b6ca
Galatée Lardière <galatee.lardiere@gmail.com>
2022-05-12 20:44:06 +0200

    form

--- frontend/src/components/AnnonceForm.jsx
+++ frontend/src/components/AnnonceForm.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import { React, useState } from "react";
+import "../pages/Annonce.css";
 
 export default function AnnonceForm() {
+  const [title, setTitle] = useState("");
+  const [share, setShare] = useState("");
+
   return (
-    <div>
-      <label className="labelform" htmlFor="name">
-        <h2 className="stringform">Nom :</h2>
-        {/* <input
+    <div className="formContainer">
+      <form>
+        <label className="labelform" htmlFor="title">
+          <h2 className="stringform">Titre de mon annonce</h2>
+          <input
             className="inputform"
             id="name"
             type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          /> */}
-      </label>
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <h2 className="stringform">Ce que je souhaites partager</h2>
+        <select name="option" id="share-select">
+          <option value="">--Choisir une catégorie de partage--</option>
+          <option value="">Artisanat</option>
+          <option value="">Cueillette</option>
+          <option value="">Energie verte</option>
+          <option value="">Explorer la nature</option>
+          <option value="">Faire des bocaux</option>
+          <option value="">Gérer un jardin</option>
+          <option value="">Hygiène</option>
+          <option value="">Textile</option>
+          <option value="">Réparations</option>
+        </select>
+        <label className="labelform" htmlFor="share">
+          <h2 className="stringform">Détail de mon annonce</h2>
+          <input
+            className="inputform"
+            id="firstName"
+            type="text"
+            value={share}
+            onChange={(e) => setShare(e.target.value)}
+          />
+        </label>
+        <input className="inputformsubmit" type="submit" value="Envoyer" />
+      </form>
     </div>
   );
 }
--- frontend/src/pages/Annonce.jsx
+++ frontend/src/pages/Annonce.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import AnnonceForm from "../components/AnnonceForm";
-import Navbar from "../components/Navbar";
+import "./Annonce.css";
 
 export default function Annonce() {
   return (
     <div>
-      <Navbar />
       Déposer une annonce
       <div className="AdForm">
-        <p>Petit texte expliquant le dépôt annonce</p>
+        <h1>Petit texte expliquant le dépôt annonce</h1>
         <AnnonceForm />
       </div>
     </div>
