03f46678bbeb0e70982f297c1a6104f7d8ce683a
misscrazy44 <mehdi.mouilbeau@hotmail.fr>
2022-05-12 20:29:25 +0200

    video creer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 import Body from "@pages/Body";
 import Footer from "@components/Footer";
 import Navbar from "@components/Navbar";
+import Video from "@components/video";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Video />
       <Body />
       <Footer />
     </div>
